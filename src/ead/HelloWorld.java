840cefac1df16f3411622de677801c51251fe965
Serhii Bielik <u5748106@au.edu>
2018-04-02 15:50:05 +0700

    error

--- src/ead/HelloWorld.java
+++ src/ead/HelloWorld.java
@@ -2,7 +2,7 @@ package ead;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World")
         System.out.println("Feature 1 Added");
     }
 }
